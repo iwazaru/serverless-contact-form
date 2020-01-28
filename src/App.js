11f47613eff6b1ddc9a79109af43ae9ba25e4c22
Clement Bourgoin <c@iwzr.fr>
2020-01-28 13:49:55 +0100

    Step 2: Build form using React

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contact me</h1>
+      <form>
+        <div className="field">
+          <label htmlFor="name">Your name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div className="field">
+          <label htmlFor="email">Your email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div className="field">
+          <label htmlFor="subject">Your subject:</label>
+          <input type="text" id="subject" name="subject" required />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Your message:</label>
+          <textarea id="message" name="message" rows="20" required></textarea>
+        </div>
+        <div className="field">
+          <button className="submit-button" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
